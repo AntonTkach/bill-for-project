61343ba892e02097ea4f1bfa5536284dd5ec1442
jelizavetavakarjuk <vakaryuk.liza@gmail.com>
2017-02-16 11:25:18 +0200

    ID

--- Bill/Models/OrderLine.cs
+++ Bill/Models/OrderLine.cs
@@ -7,6 +7,12 @@ namespace Bill.Models
 {
     public class OrderLine : BaseOrderLine
     {
+        private string orderId;
+        public string OrderID
+        {
+            get { return string.IsNullOrEmpty(orderId) ? string.Empty : orderId; }
+            set { orderId = value; }
+        }
 
     }
 }
